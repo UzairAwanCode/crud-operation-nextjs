764532edc02308e6f10f755c76772521f7e872ef
UzairAwanCode <githubprojects000@gmail.com>
2025-01-25 10:12:10 +0500

    Complete Crud Operation

--- src/app/editTopic/[id]/page.jsx
+++ src/app/editTopic/[id]/page.jsx
@@ -1,9 +1,28 @@
 import EditTopicForm from '@/components/EditTopicForm'
 import React from 'react'
 
-function page() {
+const getTopicById = async (id) => {
+    try {
+        const res = await fetch(`http://localhost:3000/api/topics/${id}`, { cache: 'no-store' })
+        if (!res.ok) {
+            throw new Error("Something Went Wrong in getTopicById function")
+        }
+
+        return res.json()
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+async function page({ params }) {
+    const { id } = await params
+    const { topic } = await getTopicById(id)
+    console.log(topic);
+
+    const { title, desc } = topic
+
     return (
-        <EditTopicForm />
+        <EditTopicForm id={id} title={title} description={desc} />
     )
 }
 
--- src/components/EditTopicForm.jsx
+++ src/components/EditTopicForm.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+"use client"
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
-function EditTopicForm() {
+function EditTopicForm({ id, title, description }) {
+    const [newTitle, setNewTitle] = useState(title)
+    const [newDesc, setNewDesc] = useState(description)
+    const router = useRouter()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ newTitle, newDesc })
+            })
+
+            if (!res.ok) {
+                throw new Error('Something Went Wrong')
+            }
+
+            router.push('/')
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
-        <form className='flex flex-col gap-3'>
-            <input className='w-full border border-slate-500 px-8 py-2' type='text' placeholder='Topic Title' />
-            <input className='w-full border border-slate-500 px-8 py-2' type='text' placeholder='Topic Description' />
+        <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+            <input value={newTitle} onChange={(e) => setNewTitle(e.target.value)} className='w-full border border-slate-500 px-8 py-2' type='text' placeholder='Topic Title' />
+            <input value={newDesc} onChange={(e) => setNewDesc(e.target.value)} className='w-full border border-slate-500 px-8 py-2' type='text' placeholder='Topic Description' />
             <button className='bg-green-600 font-bold text-white py-3 px-6 w-fit'>Update Topic</button>
         </form>
     )
